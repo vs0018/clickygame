425a079e842f42534bc8284c245377f811d45fc8
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-04-04 22:14:30 -0400

    Updates

---
+++ precache-manifest.3c2e1a915e5946a071a4916af788e1a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0146524616eea72d1b",
+    "url": "/clickygame/static/js/runtime~main.bb8261cb.js"
+  },
+  {
+    "revision": "74ca18ff982aceba1cba",
+    "url": "/clickygame/static/js/main.1e39f0c1.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "74ca18ff982aceba1cba",
+    "url": "/clickygame/static/css/main.c318d270.chunk.css"
+  },
+  {
+    "revision": "fb720121c1bf4efec5208c93e92137d3",
+    "url": "/clickygame/index.html"
+  }
+];
