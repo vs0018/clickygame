880cb88fb644cda341d9f8f235316adf6c198b3e
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-04-04 21:39:21 -0400

    get image clicks working, adding to score

--- src/App.js
+++ src/App.js
@@ -1,65 +1,92 @@
 import React, { Component } from "react";
 import ImgCard from "./components/ImgCard";
+import Nav from "./components/Nav";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import images from "./images.json";
+
+// Shuffle memory game images
+function shuffleArray(array) {
+  let i = array.length - 1;
+  for (; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
 
 class App extends Component {
   // Setting this.state.images to the images json array
   state = {
     images,
-    "score": 0,
-    "highscore": 0
-  };
-
-  // Shuffle memory game images
-  shuffleArray = array => {
-    let i = array.length - 1;
-    for (; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      const temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-    return array;
+    "currentScore": 0,
+    "topScore": 0,
+    "announce": "",
+    "clicked": []
   };
 
   resetGame = () => {
     this.setState({
-      score: 0,
-      highscore: this.state.highscore,
+      currentScore: 0,
+      topScore: this.state.topScore,
+      announce: "Sorry, try again!",
+      clicked: []
     });
     this.handleShuffle();
   };
 
   handleShuffle = () => {
-    let shuffledImgs = this.shuffleArray(images);
+    let shuffledImgs = shuffleArray(images);
     this.setState({ images: shuffledImgs });
   };
 
-  inreaseScore = () => {
-    this.setState({ score: this.state.score + 1});
-  }
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      announce: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ announce: "You win!" });
+    }
+    this.handleShuffle();
+  };
 
   // handleDecrement decreases this.state.count by 1
   handleClick = id => {
-    if (this.state.count > 0) {
-    // Decrements images click count
-    this.setState({ count: this.state.count - 1 });
-    };
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.resetGame();
+    }
   }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Clicky Game, Score: {this.state.score}</Title>
+        <Nav
+          title="Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+        <Title>Clicky Game</Title>
         {this.state.images.map(image => (
           <ImgCard
-            id={image.id}
             key={image.id}
+            handleClick={this.handleClick}
+            handleIncrement={this.handleIncrement}
+            resetGame={this.resetGame}
+            handleShuffle={this.handleShuffle}
+            id={image.id}
             image={image.image}
-            score={this.state.score}
           />
         ))}
       </Wrapper>
