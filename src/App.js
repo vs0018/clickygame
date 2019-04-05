ac539abe90d18b1dd700aa9169af5e115b97711a
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-04-04 20:00:52 -0400

    refactor code to put all logic in app.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,59 @@ import images from "./images.json";
 class App extends Component {
   // Setting this.state.images to the images json array
   state = {
-    "images": images,
+    images,
+    "score": 0,
+    "highscore": 0
   };
+
+  // Shuffle memory game images
+  shuffleArray = array => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+    return array;
+  };
+
+  resetGame = () => {
+    this.setState({
+      score: 0,
+      highscore: this.state.highscore,
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledImgs = this.shuffleArray(images);
+    this.setState({ images: shuffledImgs });
+  };
+
+  inreaseScore = () => {
+    this.setState({ score: this.state.score + 1});
+  }
+
+  // handleDecrement decreases this.state.count by 1
+  handleClick = id => {
+    if (this.state.count > 0) {
+    // Decrements images click count
+    this.setState({ count: this.state.count - 1 });
+    };
+  }
 
   // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
       <Wrapper>
-        <Title>Clicky Game</Title>
+        <Title>Clicky Game, Score: {this.state.score}</Title>
         {this.state.images.map(image => (
           <ImgCard
             id={image.id}
             key={image.id}
             image={image.image}
+            score={this.state.score}
           />
         ))}
       </Wrapper>
--- src/components/ImgCard/index.js
+++ src/components/ImgCard/index.js
@@ -1,31 +1,16 @@
-import React, { Component } from "react";
-// import "./style.css";
+import React from "react";
+import "./style.css";
 
-class ImgCard extends Component {
-  // Setting this.state.images to the images json array
-  state = {
-    count: 2
-  };
-
-  // handleDecrement decreases this.state.count by 1
-  handleDecrement = () => {
-    // We always use the setState method to update a component's state
-    this.setState({ count: this.state.count - 1 });
-  };
-
-render() {
-  return (
-      <div className="card">
-        <div className="card-body">
-          <p className="card-text">Click Count: {this.state.count}</p>
-          <div onClick={this.handleDecrement} className="img-container">
-            <img alt="" src={this.props.image} />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
+const ImgCard = props => (
+  <div 
+    className="card" 
+    value={props.id} 
+    onClick={() => props.handleClick(props.id)}
+  >
+    <div className="img-container">
+      <img alt={props.name} src={props.image} />
+    </div>
+  </div>
+);
 
 export default ImgCard;
